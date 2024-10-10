c57e6f0f716daf3625cdd72ea64d082f273f7333
minerchan <samuelcavalcantimg@gmail.com>
2024-10-09 21:58:17 -0300

    interface created

---
+++ setup.py
@@ -0,0 +1,17 @@
+import sys
+from cx_Freeze import setup, Executable
+
+
+dependecies = {"packages": ["os"], "includes": ["tkinter", "pyautogui"]}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="Goldenfish",
+    version="0.1",
+    description="A macro to make your fishing easier",
+    options={"build_exe": dependecies},
+    executables=[Executable("app.py", base=base)]
+)
